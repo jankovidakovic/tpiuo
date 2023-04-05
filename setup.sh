fbd8ec5e34da096218bf91e12c2d67c5a5a97e1f
jvidakovic <janko.vidakovic@gmail.com>
2023-04-05 12:57:14 +0200

    Fix typo

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 alias kafka-topics='docker exec $(basename $PWD)-kafka1-1 kafka-topics --bootstrap-server=kafka1:9092'
-alias run_producer='docker exec -it $(basename $PWD)-tpiuo-kafka1-1 kafka-console-producer --bootstrap-server kafka1:9092'
+alias run_producer='docker exec -it $(basename $PWD)-kafka1-1 kafka-console-producer --bootstrap-server kafka1:9092'
 alias run_consumer='docker exec -it $(basename $PWD)-kafka1-1 kafka-console-consumer --bootstrap-server kafka1:9092 --from-beginning'
