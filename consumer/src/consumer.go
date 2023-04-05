121454db5730a1aba37b9a5bdac338c8ced6f2d1
jvidakovic <janko.vidakovic@gmail.com>
2023-04-05 10:51:03 +0200

    Prototype working consumer

---
+++ consumer/src/consumer.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	fmt.Println("Hello world")
+
+	// now we want to initialize a consumer
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "127.0.0.1:9092",
+		"group.id":          "bitcoin",
+		"auto.offset.reset": "earliest"})
+
+	if err != nil {
+		fmt.Printf("Failed to create consumer: %s", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Successfully created consumer!")
+
+	topic := "bitcoin"
+	err = consumer.SubscribeTopics([]string{topic}, nil)
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	run := true
+	for run {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev, err := consumer.ReadMessage(100 * time.Millisecond)
+			if err != nil {
+				continue
+			}
+			fmt.Printf("Consumed event from topic %s: key = %-10s value = %s\n",
+				*ev.TopicPartition.Topic, string(ev.Key), string(ev.Value))
+		}
+	}
+
+	consumer.Close()
+	fmt.Println("Finished consuming the topic.")
+}
